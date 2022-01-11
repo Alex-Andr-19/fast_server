609051a253e4b690cb5f650e3841c530b2a3243c
Andrew <andrey.aleksandrychev2@mail.ru>
2022-01-11 18:37:47 +0300

    Add DB

--- server.js
+++ server.js
@@ -2,9 +2,31 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get("/data", (req, res) => {
+const sqlite3 = require('sqlite3').verbose()
+let db = new sqlite3.Database('./db.db')
+
+app.get("/dataStatic", (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.sendFile(__dirname + "/db.json")
+})
+app.get("/dataSQLite", (req, res) => {
+    let sql = 'SELECT * FROM studentDB'
+
+    let resObj = {
+        res: {},
+        status: false
+    }
+
+    db.all(sql, [], (er, rows) => {
+        if (er) {
+            console.log(er)
+        } else {
+            resObj.status = true
+            resObj.res = rows
+        }
+
+        res.json(resObj)
+    })
 })
 
 app.listen(port, () => {
