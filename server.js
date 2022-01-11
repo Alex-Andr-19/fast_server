850d6679bef60f2a43f12c7c531989918fb4b7cd
Andrew <andrey.aleksandrychev2@mail.ru>
2022-01-11 06:38:37 +0300

    Fix comimit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get("/data", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.sendFile(__dirname + "/db.json")
 })
 
